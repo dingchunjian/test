3b0bdcb4ddcc7c2a2e955af70cd6bb7b5a4c451e
dingchunjian <952179760@qq.com>
2020-07-02 16:56:35 +0800

    构建项目

---
+++ src/views/Login/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="login">
+     <el-button type="primary">hello</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.login{ 
+    background: #344a5f;
+    height: 100vh;
+}
+</style>
